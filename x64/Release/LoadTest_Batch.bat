a31e457bc94a538d603edefa47f83e198e9bc852
odavis2807 <145225318+odavis2807@users.noreply.github.com>
2026-04-14 18:42:55 -0400

    Modify LoadTest_Batch.bat for increased client instances
    Updated LoadTest_Batch.bat to run 25 instances of Client.exe with specific parameters and increased count from 5 to 25.

--- x64/Release/LoadTest_Batch.bat
+++ x64/Release/LoadTest_Batch.bat
@@ -1,12 +1,11 @@
 @echo off
 SET /A "index = 1"
-SET /A "count = 5"
-
+SET /A "count = 25"
 :while
 if %index% leq %count% (
-     START /MIN Client.exe
+     START /MIN Client.exe 25.21.209.204 "Telem_2023_3_12 16_26_4.txt"
      SET /A index = %index% + 1
      @echo %index%
+     timeout /t 1 /nobreak > NUL
      goto :while
 )
-
